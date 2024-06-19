85c77fb714c80cf61f5c829005b23cf71dfaa46c
Sanchir <sanchir_tumentsetseg@hotmail.com>
2024-06-20 00:46:30 +0200

    jag vet inte

---
+++ script/home.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".mobil-link");
+    const icon = document.querySelector(".mobil-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
